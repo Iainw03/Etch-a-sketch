0391e25bcd9276ae22572231ac098794da9a1c04
Iainw03 <143198825+Iainw03users.noreply.github.com>
2024-07-12 20:17:16 +0100

    number of grid boxes can now be changed and still be contained within the same space

--- javascript.js
+++ javascript.js
@@ -1,25 +1,29 @@
 function CreateDiv(divs){
+    
     const container = document.querySelector("#container");
 for(let i = 0;i<divs;i++){
 const content = document.createElement("div");
 content.classList.add("grid");
+content.style.height = (100 / divs) + "%";
 container.appendChild(content);
 
-for(let j=0;j<16;j++){
+for(let j=0;j<divs;j++){
 const row = document.createElement("div");
 row.classList.add("rows");
+row.style.width = (100 / divs) + "%";
 content.appendChild(row);
+
 }
 }
 
 }
 
-CreateDiv(16);
+CreateDiv(18);
 
 const grids = document.querySelectorAll(".rows");
 grids.forEach(grid => {
     grid.addEventListener("mouseover", () => {
-        grid.setAttribute("style", "background-color: black;");
+        grid.style.backgroundColor="black";
     });
 });
 
