af237679fca190bd62ef91de82ffb4cb8c77d702
Iainw03 <143198825+Iainw03users.noreply.github.com>
2024-07-13 00:03:50 +0100

    Added proper descriptions on prompt and button. -Removed border lines on board. -New boards can now be drawn on

--- javascript.js
+++ javascript.js
@@ -18,14 +18,45 @@ content.appendChild(row);
 
 }
 
-CreateDiv(18);
 
-const grids = document.querySelectorAll(".rows");
+function addMouseover(){
+    const grids = document.querySelectorAll(".rows");
 grids.forEach(grid => {
     grid.addEventListener("mouseover", () => {
         grid.style.backgroundColor="black";
     });
 });
+}
+
+
+CreateDiv(18);
+
+addMouseover();
+
+
+
+const btn = document.querySelector("#gridbutton");
+
+    btn.addEventListener("click", () =>{
+        container.innerHTML = " ";
+        let divs = prompt("Choose a grid size(number between 1 and 100)");
+
+        if(divs>100){
+            CreateDiv(100);
+            addMouseover();
+        }
+        else{
+        CreateDiv(divs);
+        addMouseover();
+        }
+
+        const grids = document.querySelectorAll(".rows");
+
+
+    });
+    //parentNode.removeChild(child)
+
+  
 
 
 
